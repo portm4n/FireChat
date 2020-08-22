da9b1cec9cf9718b2894c74327834d2e5bc2ac96
portm4n <ruben_sanfer_3@hotmail.com>
2020-08-22 07:26:55 +0200

    inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule} from 'angularfire2'
+import { AngularFirestoreModule} from 'angularfire2/firestore'
+import { AngularFireAuthModule} from 'angularfire2/auth'
+import { environment } from '../environments/environment';
+import { ChatComponent } from './components/chat/chat.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAEkghLkh8kZj80cDzYmbu87cJkPsK8jLM",
+    authDomain: "firechat-f98c0.firebaseapp.com",
+    databaseURL: "https://firechat-f98c0.firebaseio.com",
+    projectId: "firechat-f98c0",
+    storageBucket: "firechat-f98c0.appspot.com",
+    messagingSenderId: "655066802484",
+    appId: "1:655066802484:web:459bbc867c04fe2fda9d61",
+    measurementId: "G-ZKDCYX97JW"
+  }
 };
 
 /*
